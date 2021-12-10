bb5b058861c6f4ebdaaef89c1924767d39f6f6c9
mahimtalukder <78541907+mahimtalukder@users.noreply.github.com>
2021-12-10 23:16:41 +0600

    sign out add

---
+++ sign_out.php
@@ -0,0 +1,12 @@
+<?php 
+	session_start();
+
+	if (isset($_SESSION['username'])) {
+		session_destroy();
+		header("location: sign-in.php");
+	}
+	else{
+		header("location: sign-in.php");
+	}
+
+ ?>
